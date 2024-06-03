c541ba85f9377e61233b7ce0b6f3c7600ca597c7
Lusca <lucaspacheco747@gmail.com>
2024-06-03 11:47:41 -0300

    Tela de Login

---
+++ front-end/sociedadeIntergalatica/src/pages/Login/index.jsx
@@ -0,0 +1,89 @@
+import style from "./Login.module.css";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import toastr from "toastr";
+
+
+function Login() {
+    const [usrLogin, setusrLogin] = useState("");
+    const [password, setpassword] = useState("");
+  
+    const navigate = useNavigate();
+  
+    const successCb = (user) => {
+      console.log(user);
+      document.cookie = `userToken=${user.token}`;
+      navigate("/clientes");
+    };
+  
+    const errorCb = (error) => {
+      toastr.error('Erro ao realizar login');
+      console.log(error);
+    };
+  
+    const doLogin = async () => {
+      return
+      navigate("/clientes");
+  
+  
+      const user = {
+        login: usrLogin,
+        password: password,
+      };
+  
+      const login = await fetch(endpoints.login, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+  
+      const data = await login.json();
+  
+      if (login.status === 200) {
+        successCb(data);
+        return;
+      }
+  
+      if(login.status === 401){
+        toastr.warning("Login inválido!");
+        return;  
+      }
+  
+      errorCb(data);
+    };
+  
+    return (
+      <div className={style.container}>
+        <figure>
+          <img src={logo} alt="Logo" />
+        </figure>
+        <div className={style.group}>
+          <label htmlFor="login">Login</label>
+          <input
+            type="text"
+            id="login"
+            onChange={(e) => setusrLogin(e.target.value)}
+            value={usrLogin}
+            onKeyDown={e => { e.key === 'Enter' && doLogin() }}
+          />
+        </div>
+        <div className={style.group}>
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            onChange={(e) => setpassword(e.target.value)}
+            value={password}
+            onKeyDown={e => { e.key === 'Enter' && doLogin() }}
+          />
+        </div>
+        <button onClick={doLogin}>Entrar</button>
+      </div>
+    );
+  }
+  export default Login;
+  
